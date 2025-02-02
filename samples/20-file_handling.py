2b8105ba6af29ddba1d99615008d2a3824fb7ae2
mohamed eraki <mohamed-ibrahim2021@outlook.com>
2025-02-02 18:12:16 +0200

    file handling 01

---
+++ samples/20-file_handling.py
@@ -0,0 +1,13 @@
+import os
+
+print(os.getcwd())  # get current working directory 
+# that the python search for the next command
+file = open("README.md")
+
+print(os.path.abspath(__file__))  # return the absoulte path of the current file, __file__ refers to the current file.
+print(os.path.dirname(os.path.abspath(__file__))) # print the working directory of the current file.
+
+# Change current working dire
+print("#" * 50)
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+print(os.getcwd())
