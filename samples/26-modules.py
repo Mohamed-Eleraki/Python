63aee417fa7b61014979d24852d2c34eea5fd7f7
mohamed eraki <mohamed-ibrahim2021@outlook.com>
2025-02-18 17:38:28 +0200

    change files permissoins

---
+++ samples/26-modules.py
@@ -0,0 +1,11 @@
+# all modules included into functools.py        /usr/lib/python3.12/functools.py
+
+# ls /usr/lib/python3.12 | grep all
+# compileall.py
+# tracemalloc.py
+
+# ls /usr/lib/python3.12/ | grep random
+# random.py
+
+import random
+print(random)  # get the module path
