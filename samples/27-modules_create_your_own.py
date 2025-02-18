3b7ad757543671dad098f2de63f3d49345e869f0
mohamed eraki <mohamed-ibrahim2021@outlook.com>
2025-02-18 17:56:40 +0200

    modules

---
+++ samples/27-modules_create_your_own.py
@@ -0,0 +1,10 @@
+# 1 - create a file with .py extention
+# 2 - type your functions in it
+def say_hello(name):
+    print('Hello {name}')
+
+def how_are_you(name):
+    print('How are you {name}')
+    
+# 3 - in your py file import it
+import 
